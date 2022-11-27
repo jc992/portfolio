f1016685f00859c9ba644656018b68d3f6fcc8c7
ffmcgee <jc.992@hotmail.com>
2022-11-27 20:49:58 -0300

    export 'assets' from projects into separate utils file.

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Card from "../components/Card";
-import apy_vision from "../assets/apy_vision.svg";
-import apy_vision_landing from "../assets/apy_vision_landing.svg";
-import ohne from "../assets/ohne.svg";
-import olympus from "../assets/olympus.svg";
-import data_talks from "../assets/data_talks.svg";
-import spice_landing_page from "../assets/spice_landing_page.svg";
 import { TERTIARY_COLOR } from "../components/common";
+import assets from "../utils/assets";
 
 const Grid = styled.div`
   max-width: 1100px;
@@ -26,111 +21,23 @@ const H2 = styled.h2`
   color: ${TERTIARY_COLOR};
 `;
 
-const Projects = () => {
-  const assets = [
-    {
-      title: "APY.Vision Web App",
-      paragraph: "DeFi data platform for tracking liquidity pool gains and impermanent loss, also providing information useful for entering and exiting liquidity pools or yield farms.",
-      image: apy_vision,
-      stack: [
-        "Typescript",
-        "Nest.js",
-        "TypeORM",
-        "React.js",
-        "Next.js",
-        "Web3.js",
-        "Redux",
-        "Ruby on Rails",
-      ],
-      link: "https://app.apy.vision/",
-    },
-    {
-      title: "Olympus DAO Web App",
-      paragraph: "Front End Web App for a Decentralized Reserve Cryptocurrency known as Olympus (open source project with decentralized work methodology).",
-      image: olympus,
-      stack: [
-        "Typescript",
-        "React-Query",
-        "Next.js",
-        "React.js",
-        "Redux",
-        "Ethers.js",
-      ],
-      github: "https://github.com/OlympusDAO/olympus-frontend",
-      link: "https://app.olympusdao.finance/",
-    },
-    {
-      title: "Spice Finance Landing Page",
-      paragraph: "Marketing landing page for an Aggregated NFT lending liquidity protocol known as Spice Finance.",
-      image: spice_landing_page,
-      stack: [
-      "Javascript",
-      "Vite",
-      "React.js",
-      "Styled-Components",
-      "Supabase",
-      ],
-      github: "https://github.com/jc992/spice-landing-page-v1",
-      link: "https://spice-landing-page-v1.vercel.app/",
-      },
-    {
-      title: "APY.Vision Landing Page",
-      paragraph: "Marketing landing page with the purpose of guiding users to the main Web App of APY.Vision.",
-      image: apy_vision_landing,
-      stack: [
-       "Javascript",
-       "React.js",
-       "SASS",
-      ],
-      link: "https://apy.vision/",
-     },
-     {
-      title: "Ohne",
-      paragraph: "E-Commerce platform for 100% organic period related products.",
-      image: ohne,
-      stack: [
-        "Typescript",
-        "Express.js",
-        "Knex.js",
-        "GraphQl",
-        "Gatbsy.js",
-        "React.js",
-      ],
-      link: "https://ohne.com/",
-    },
-     {
-      title: "DataTalks CDP",
-      paragraph: "Customer data platform with the purpose of segmenting customers into audiences with similar characteristics in order to achieve a more accurate and personalized communication.",
-      image: data_talks,
-      stack: [
-       "C#",
-       ".NET Core",
-       "HTML5",
-       "SASS",
-       "Javascript",
-       "PostgreSQL",
-       "Git",
-      ],
-     },
-  ];
-  return (
-    <div id="projects">
-      <H2>projects</H2>
-      <Grid>
-        {assets.map(({ title, paragraph, image, stack, link, github }, i) => (
-          <Card
-            key={i}
-            title={title}
-            paragraph={paragraph}
-            image={image}
-            stack={stack}
-            github={github}
-            link={link}
-          />
-        ))}
-      </Grid>
-    </div>
-  );
-};
+const Projects = () => (
+  <div id="projects">
+    <H2>projects</H2>
+    <Grid>
+      {assets.map(({ title, paragraph, image, stack, link, github }, i) => (
+        <Card
+          key={i}
+          title={title}
+          paragraph={paragraph}
+          image={image}
+          stack={stack}
+          github={github}
+          link={link}
+        />
+      ))}
+    </Grid>
+  </div>
+);
 
 export default Projects;
